8da338b4756a13e7c6c8bf64613bb3a9ce3430f4
Johan Carleborn <johancarleborn@gmail.com>
2022-12-23 12:58:16 +0100

    removed profile page stuff

--- setup/scripts.php
+++ setup/scripts.php
@@ -2,11 +2,11 @@
 // Enqueue scripts and styles.
 
 function lightning_scripts() {
-	wp_enqueue_style( 'lightning-style', get_stylesheet_uri(), array(), _S_VERSION );
+    wp_enqueue_style('lightning-style', get_stylesheet_uri(), array(), _S_VERSION);
 
     // CSS
     wp_enqueue_style('lw-app-css', get_template_directory_uri() . '/dist/app.css', filemtime(get_template_directory() . '/dist/app.css'));
-    
+
     // JS
     wp_enqueue_script('lw-app-js', get_template_directory_uri() . '/dist/app.js', array('jquery'), filemtime(get_template_directory() . '/dist/app.js'), true);
 
@@ -18,28 +18,26 @@ function lightning_scripts() {
         'restBase' => get_rest_url() . 'lw',
         'loadMore' => wp_create_nonce('load_more'),
         'filterSearch' => wp_create_nonce('filter_search'),
-        'updateProfile' => wp_create_nonce('update_profile'),
-        'deleteProfile' => wp_create_nonce('delete_profile'),
         'isLoggedIn' => is_user_logged_in(),
     ));
 
-    if ( is_singular() && comments_open() && get_option( 'thread_comments' ) ) {
-		wp_enqueue_script( 'comment-reply' );
-	}
+    if (is_singular() && comments_open() && get_option('thread_comments')) {
+        wp_enqueue_script('comment-reply');
+    }
 }
-add_action( 'wp_enqueue_scripts', 'lightning_scripts' );
+add_action('wp_enqueue_scripts', 'lightning_scripts');
 
 function admin_scripts() {
     wp_enqueue_style('material-icons', 'https://fonts.googleapis.com/icon?family=Material+Icons+Round');
-	wp_enqueue_style('lightning-style', get_stylesheet_directory_uri() . '/admin/lightning-builder.css');
-	wp_enqueue_script('admin-script-js', get_stylesheet_directory_uri() . '/admin/admin.js', null, null, true);
+    wp_enqueue_style('lightning-style', get_stylesheet_directory_uri() . '/admin/lightning-builder.css');
+    wp_enqueue_script('admin-script-js', get_stylesheet_directory_uri() . '/admin/admin.js', null, null, true);
 }
 add_action('admin_enqueue_scripts', 'admin_scripts', 999);
 
-function remove_wp_block_library_css(){
-    wp_dequeue_style( 'wp-block-library' );
-    wp_dequeue_style( 'wp-block-library-theme' );
-    wp_dequeue_style( 'wc-blocks-style' ); // Remove WooCommerce block CSS
-    wp_dequeue_style( 'global-styles' );
+function remove_wp_block_library_css() {
+    wp_dequeue_style('wp-block-library');
+    wp_dequeue_style('wp-block-library-theme');
+    wp_dequeue_style('wc-blocks-style'); // Remove WooCommerce block CSS
+    wp_dequeue_style('global-styles');
 }
-add_action( 'wp_enqueue_scripts', 'remove_wp_block_library_css', 100 );
+add_action('wp_enqueue_scripts', 'remove_wp_block_library_css', 100);
