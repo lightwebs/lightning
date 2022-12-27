51ba78468cd898f7163049243c97c970dc889db3
Erik Näslund <contact@eriknaslund.com>
2022-12-27 12:58:47 +0100

    Init component

---
+++ inc/acf/lightning-builder/page-components/pc-project-cta.php
@@ -0,0 +1,17 @@
+<?php
+if (get_row_layout() == 'project_cta' && !s(get_row_layout())['hide_component']) :
+    $prefix = get_row_layout();
+    $text = get_sub_field('text');
+    $url = get_sub_field('url');
+    $icon =  get_stylesheet_directory_uri() . '/src/assets/icons/arrow-right.svg'
+?>
+    <section id="<?php echo s($prefix)['component_id']; ?>" class="pc-project_cta section <?php echo section_spacing(); ?> <?php echo s($prefix)['bg_color']; ?>">
+
+        <?php component_header($prefix); ?>
+        <a href="<?php echo $url; ?>">
+            <div class=" <?php echo s($prefix)['text_color']; ?> flex items-end pb-6 pl-10 w-full  max-w-[549px]">
+                <h1 class="m-0 "><?php echo $text; ?></h1>
+            </div>
+        </a>
+    </section>
+<?php endif; ?>
---
+++ inc/acf/lightning-builder/reg-components/project-cta.php
@@ -0,0 +1,76 @@
+<?php
+return [
+    'key' => 'layout_project_cta',
+    'name' => 'project_cta',
+    'label' => 'Project CTA',
+    'display' => 'block',
+    'sub_fields' => [
+        [
+            'key' => 'field_lb_project_cta_comp_head_tab',
+            'label' => 'Komponenthuvud',
+            'type' => 'tab',
+            'placement' => 'top',
+        ],
+        [
+            'key' => 'field_lb_project_cta_header',
+            'label' => 'Header',
+            'name' => 'project_cta',
+            'type' => 'clone',
+            'clone' => [
+                0 => 'group_clone_component_header',
+            ],
+            'display' => 'seamless',
+            'prefix_name' => 1
+        ],
+        [
+            'key' => 'field_lb_project_cta_content_tab',
+            'label' => 'Innehåll',
+            'type' => 'tab',
+            'placement' => 'top',
+        ],
+        [
+            'key' => 'field_lb_project_cta_text',
+            'label' => __('Text', 'lightning'),
+            'name' => 'text',
+            'type' => 'text',
+            'instructions' => __('This is the text displayed in the CTA', 'lightning'),
+            'required' => 0, // 0 | 1
+            'wrapper' => ['width' => 100] // 0-100
+        ],
+        [
+            'key' => 'field_lb_project_cta_url',
+            'label' => __('Url', 'lightning'),
+            'name' => 'url',
+            'type' => 'url',
+            'instructions' => __('The URL the CTA should link to', 'lightning'),
+            'required' => 0, // 0 | 1
+            'wrapper' => ['width' => 100] // 0-100
+        ],
+        [
+            'key' => 'field_lb_project_cta_settings_tab',
+            'label' => 'Inställningar',
+            'type' => 'tab',
+            'placement' => 'top',
+        ],
+        [
+            'key' => 'field_lb_project_cta_colors',
+            'label' => 'Färger',
+            'name' => 'project_cta',
+            'type' => 'clone',
+            'clone' => [
+                0 => 'group_clone_component_footer',
+            ],
+            'display' => 'seamless',
+            'prefix_name' => 1
+        ],
+        [
+            'key' => 'field_lb_project_cta_component_id',
+            'label' => 'Komponentens id (frivillig)',
+            'name' => 'component_id',
+            'type' => 'text',
+            'instructions' => $component_id_instructions,
+            'placeholder' => 'my-id',
+            'prepend' => '#',
+        ]
+    ]
+];
