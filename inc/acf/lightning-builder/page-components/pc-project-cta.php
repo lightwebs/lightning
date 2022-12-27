123a6a415fe8725953a56a6ee23e74c37e92d8fc
Erik Näslund <contact@eriknaslund.com>
2022-12-27 14:05:58 +0100

    Make anchor wrap text only

--- inc/acf/lightning-builder/page-components/pc-project-cta.php
+++ inc/acf/lightning-builder/page-components/pc-project-cta.php
@@ -4,15 +4,14 @@ if (get_row_layout() == 'project_cta' && !s(get_row_layout())['hide_component'])
     $text = get_sub_field('text');
     $url = get_sub_field('url');
 ?>
-    <section id="<?php echo s($prefix)['component_id']; ?>" class="pc-project_cta section <?php echo section_spacing(); ?> <?php echo s($prefix)['bg_color']; ?>">
+    <section id="<?php echo s($prefix)['component_id']; ?>" class="pc-project_cta section  <?php echo section_spacing(); ?> <?php echo s($prefix)['bg_color']; ?>">
         <?php component_header($prefix); ?>
-        <a href="<?php echo $url; ?>">
-            <div class=" <?php echo s($prefix)['text_color']; ?> flex items-end lg py-9 md:py-12 px-4 md:px-20 w-full ">
-                <h1 class="m-0  max-w-[550px]"><?php echo $text; ?> <span class="material-icons-round text-[30px]">
+        <div class=" <?php echo s($prefix)['text_color']; ?> flex items-end w-full md:h-[360px]">
+            <a href="<?php echo $url; ?>" class="py-9 md:py-12 px-4 md:px-20">
                         arrow_forward
                     </span></h1>
 
-            </div>
-        </a>
+            </a>
+        </div>
     </section>
 <?php endif; ?>
