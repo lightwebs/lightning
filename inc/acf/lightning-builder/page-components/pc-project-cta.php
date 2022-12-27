f1c4322a136ce1f4e2d4efe1fa98f3f9e4d4d8b1
Erik Näslund <contact@eriknaslund.com>
2022-12-27 14:00:17 +0100

    Add material icon

--- inc/acf/lightning-builder/page-components/pc-project-cta.php
+++ inc/acf/lightning-builder/page-components/pc-project-cta.php
@@ -5,12 +5,13 @@ if (get_row_layout() == 'project_cta' && !s(get_row_layout())['hide_component'])
     $url = get_sub_field('url');
 ?>
     <section id="<?php echo s($prefix)['component_id']; ?>" class="pc-project_cta section <?php echo section_spacing(); ?> <?php echo s($prefix)['bg_color']; ?>">
-
         <?php component_header($prefix); ?>
         <a href="<?php echo $url; ?>">
-            <div class=" <?php echo s($prefix)['text_color']; ?> flex items-end pb-6 pl-10 w-full  max-w-[549px]">
-                <h1 class="m-0 "><?php echo $text; ?></h1>
-            <div class=" <?php echo s($prefix)['text_color']; ?> flex items-end pb-6 pl-10 w-full  max-w-[550px]">
+            <div class=" <?php echo s($prefix)['text_color']; ?> flex items-end lg py-9 md:py-12 px-4 md:px-20 w-full ">
+                <h1 class="m-0  max-w-[550px]"><?php echo $text; ?> <span class="material-icons-round text-[30px]">
+                        arrow_forward
+                    </span></h1>
+
             </div>
         </a>
     </section>
