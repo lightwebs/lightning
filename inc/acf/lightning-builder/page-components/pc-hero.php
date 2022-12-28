9bfe2c127ed3570cc787dfb8d841cd09c5384f57
Erik Näslund <contact@eriknaslund.com>
2022-12-28 14:10:53 +0100

    Add hero

---
+++ inc/acf/lightning-builder/page-components/pc-hero.php
@@ -0,0 +1,62 @@
+<?php
+if (get_row_layout() == 'hero' && !s(get_row_layout())['hide_component']) :
+    $prefix = get_row_layout();
+    $title = get_sub_field('title');
+    $subtitle = get_sub_field('subtitle');
+    $description = get_sub_field('description');
+    $image = get_sub_field('image');
+    $key_info = get_sub_field('key_info_repeater');
+    $buttons = get_sub_field('buttons_repeater');
+?>
+    <section id="<?php echo s($prefix)['component_id']; ?>" class="pc-hero section <?php echo section_spacing(); ?> <?php echo s($prefix)['bg_color']; ?>">
+
+        <?php component_header($prefix); ?>
+
+        <div class="container <?php echo s($prefix)['text_color']; ?> flex flex-col md:flex-row">
+            <div class="w-full">
+                <h3 class="text-purple-500"><?= $subtitle; ?></h3>
+                <h1><?= $title; ?></h1>
+                <p><?= $description; ?></p>
+                <div class="flex gap-4">
+                    <?php
+                    if (have_rows('key_info_repeater')) :
+                        while (have_rows('key_info_repeater')) : the_row();
+                            $title = get_sub_field('key_info_title');
+                    ?>
+                            <span class="material-icons-round text-purple-500">change_history</span>
+                            <p> <?= $title ?> </p>
+                    <?php
+                        endwhile;
+                    endif;
+                    ?>
+
+                </div>
+                <div class="flex gap-4 items-stretch">
+
+                    <?php
+                    if (have_rows('buttons_repeater')) :
+                        while (have_rows('buttons_repeater')) : the_row();
+                            $text = get_sub_field('button_text');
+                            $show_icon = get_sub_field('show_icon');
+                            $is_external = get_sub_field('is_external');
+                            $type = get_sub_field('button_type');
+                            $button = [
+                                'url' =>  $button_url,
+                                'title' => $show_icon ? $text . '<span class="material-icons-round">arrow_forward</span>' : $text,
+                            ];
+                            $type == 'primary' ? btn_l_primary($button, 'items-center') : btn_l_secondary($button, ' justify-center text-purple-500 border-solid border-purple-500 hover:text-white border-box');
+                        endwhile;
+                    endif;
+                    ?>
+                </div>
+
+            </div>
+            <div class="w-full">
+                <? if (!empty($image)) : ?>
+                    <img src="<?= $image['url']; ?>" alt="<?= $image['alt']; ?>" />
+                <? endif; ?>
+            </div>
+
+        </div>
+    </section>
+<?php endif; ?>
