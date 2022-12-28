ee9077e8ed86e7e04ae88681714710045da6109c
Erik Näslund <contact@eriknaslund.com>
2022-12-28 14:51:38 +0100

    Update spacing

--- inc/acf/lightning-builder/page-components/pc-hero.php
+++ inc/acf/lightning-builder/page-components/pc-hero.php
@@ -12,26 +12,28 @@ if (get_row_layout() == 'hero' && !s(get_row_layout())['hide_component']) :
 
         <?php component_header($prefix); ?>
 
-        <div class="container <?php echo s($prefix)['text_color']; ?> flex flex-col md:flex-row">
-            <div class="w-full">
+        <div class="container <?php echo s($prefix)['text_color']; ?>  gap-4 md:flex">
+            <div class="w-full flex flex-col gap-4 md:gap-6">
                 <h3 class="text-purple-500"><?= $subtitle; ?></h3>
                 <h1><?= $title; ?></h1>
                 <p><?= $description; ?></p>
-                <div class="flex gap-4">
+                <div class="flex gap-5">
                     <?php
                     if (have_rows('key_info_repeater')) :
                         while (have_rows('key_info_repeater')) : the_row();
                             $title = get_sub_field('key_info_title');
                     ?>
-                            <span class="material-icons-round text-purple-500">change_history</span>
-                            <p> <?= $title ?> </p>
+                            <div class="flex gap-2 md:gap-4">
+                                <span class="material-icons-round text-purple-500">change_history</span>
+                                <p class="font-medium"> <?= $title ?> </p>
+                            </div>
                     <?php
                         endwhile;
                     endif;
                     ?>
 
                 </div>
-                <div class="flex gap-4 items-stretch">
+                <div class="flex flex-col md:flex-row gap-4 md:gap-6 md:items-stretch items-start">
 
                     <?php
                     if (have_rows('buttons_repeater')) :
@@ -41,18 +43,18 @@ if (get_row_layout() == 'hero' && !s(get_row_layout())['hide_component']) :
                             $is_external = get_sub_field('is_external');
                             $type = get_sub_field('button_type');
                             if ($show_icon) {
-                                $link['title'] = $link['title'] . '<span class="material-icons-round text-purple-500">arrow_forward</span>';
+                                $link['title'] = $link['title'] . '<span class="material-icons-round text-purple-500 group-hover:text-white transition-color duration-300">arrow_forward</span>';
                             }
-                            $type == 'primary' ? btn_l_primary($link, 'items-center') : btn_l_secondary($link, ' justify-center text-purple-500 border-solid border-purple-500 hover:text-white border-box');
+                            $type == 'primary' ? btn_l_primary($link, 'items-center', 'group flex gap-4') : btn_l_secondary($link, ' justify-center text-purple-500 border-solid border-purple-500 hover:text-white border-box group flex gap-4');
                         endwhile;
                     endif;
                     ?>
                 </div>
 
             </div>
-            <div class="w-full">
+            <div class="w-full mt-4 md:mt-0 ">
                 <? if (!empty($image)) : ?>
-                    <img src="<?= $image['url']; ?>" alt="<?= $image['alt']; ?>" />
+                    <img src="<?= $image['url']; ?>" alt="<?= $image['alt']; ?>" class="h-full object-cover" />
                 <? endif; ?>
             </div>
 
