52386f7d22d8f1b2f4ae32801ac30ffebb47c4af
Erik Näslund <contact@eriknaslund.com>
2022-12-28 10:04:40 +0100

    Change height to min-height

--- inc/acf/lightning-builder/page-components/pc-project-cta.php
+++ inc/acf/lightning-builder/page-components/pc-project-cta.php
@@ -6,7 +6,7 @@ if (get_row_layout() == 'project_cta' && !s(get_row_layout())['hide_component'])
 ?>
     <section id="<?php echo s($prefix)['component_id']; ?>" class="pc-project_cta section  <?php echo section_spacing(); ?> <?php echo s($prefix)['bg_color']; ?>">
         <?php component_header($prefix); ?>
-        <div class=" <?php echo s($prefix)['text_color']; ?> flex items-end w-full md:h-[360px]">
+        <div class=" <?php echo s($prefix)['text_color']; ?> flex items-end w-full md:min-h-[360px]">
             <a href="<?php echo $url; ?>" class="py-9 md:py-12 px-4 md:px-20">
                 <h1 class="m-0  max-w-[550px] uppercase"><?php echo $text; ?> <span class="material-icons-round text-[30px]">
                         arrow_forward
