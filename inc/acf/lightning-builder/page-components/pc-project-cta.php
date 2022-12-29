8d04b6316c906d6839ce4b7ba398d10986b36e74
Erik Näslund <contact@eriknaslund.com>
2022-12-29 14:41:28 +0100

    Update spacing
    Remove px and add container
    Make max w to 50% on sizes md and above

--- inc/acf/lightning-builder/page-components/pc-project-cta.php
+++ inc/acf/lightning-builder/page-components/pc-project-cta.php
@@ -6,9 +6,8 @@ if (get_row_layout() == 'project_cta' && !s(get_row_layout())['hide_component'])
     <section id="<?php echo s($prefix)['component_id']; ?>" class="pc-project_cta section  <?php echo section_spacing(); ?> <?php echo s($prefix)['bg_color']; ?>">
         <?php component_header($prefix); ?>
         <div class=" <?php echo s($prefix)['text_color']; ?> flex items-end w-full md:min-h-[360px]">
-            <div class="py-9 md:py-12 px-4 md:px-20"><?php custom_link($link, '
+            <div class="container py-9 md:py-12 md:w-1/2"><?php custom_link($link, '
             !inline-block text-4xl md:text-5xl m-0 uppercase'); ?></div>
-
         </div>
     </section>
 <?php endif; ?>
