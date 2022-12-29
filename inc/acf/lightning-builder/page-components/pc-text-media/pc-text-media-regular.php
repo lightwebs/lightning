cc0cb56c374f609d09c8fd6b46fc792f254099f5
Erik Näslund <contact@eriknaslund.com>
2022-12-29 15:49:27 +0100

    Load counter if statistics

--- inc/acf/lightning-builder/page-components/pc-text-media/pc-text-media-regular.php
+++ inc/acf/lightning-builder/page-components/pc-text-media/pc-text-media-regular.php
@@ -1,5 +1,9 @@
 <?php
-if ($media_type === 'img' || $media_type === 'video') :
+$is_media = $media_type === 'img';
+$is_video = $media_type === 'video';
+$is_statistics = $media_type === 'statistics';
+
+if ($is_media || $is_video || $is_statistics) :
     $img = get_sub_field('text_media_img');
     $video = get_sub_field('text_media_video');
     $video_controls = get_sub_field('text_media_video_controls');
@@ -14,7 +18,6 @@ if ($media_type === 'img' || $media_type === 'video') :
                 <div class="order-2 md:order-<?php echo $text_placement; ?>">
                     <div class="<?php echo $full_width ? 'py-4 md:py-12 md:py-20' : ''; ?>">
                         <?php echo $text; ?>
-
                         <?php if ($link) :
                             btn_l_primary($link, 'mt-4 lg:mt-6');
                         endif; ?>
@@ -23,7 +26,12 @@ if ($media_type === 'img' || $media_type === 'video') :
             <?php endif; ?>
 
             <?php if (!$full_width) : ?>
-                <?php include __DIR__ . '/pc-media.php'; ?>
+                <?php if ($is_media || $is_video) : ?>
+                    <?php include __DIR__ . '/pc-media.php'; ?>
+                <?php endif; ?>
+                <?php if ($is_statistics) : ?>
+                    <?php include __DIR__ . '/pc-counter.php'; ?>
+                <?php endif; ?>
             <?php else : ?>
                 <div class="py-0 my-0 order-1 md:order-<?php echo $text_placement === '1' ? '2' : '1'; ?>"></div>
             <?php endif; ?>
