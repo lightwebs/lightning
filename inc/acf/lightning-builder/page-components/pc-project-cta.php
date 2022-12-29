6b38235e8df23e284aa2efba27068a76ce2b2f07
Erik Näslund <contact@eriknaslund.com>
2022-12-29 16:50:59 +0100

    Update link

--- inc/acf/lightning-builder/page-components/pc-project-cta.php
+++ inc/acf/lightning-builder/page-components/pc-project-cta.php
@@ -4,10 +4,9 @@ if (get_row_layout() == 'project_cta' && !s(get_row_layout())['hide_component'])
     $link = get_sub_field('link');
 ?>
     <section id="<?php echo s($prefix)['component_id']; ?>" class="pc-project_cta section  <?php echo section_spacing(); ?> <?php echo s($prefix)['bg_color']; ?>">
-        <?php component_header($prefix); ?>
         <div class=" <?php echo s($prefix)['text_color']; ?> flex items-end w-full md:min-h-[360px]">
-            <div class="container py-9 md:py-12 md:w-1/2"><?php custom_link($link, '
-            !inline-block text-4xl md:text-5xl m-0 uppercase'); ?></div>
+            <div class="container py-9 md:py-12 "><?php custom_link($link, '
+            text-4xl md:text-5xl m-0 uppercase md:w-1/2', true, 4); ?></div>
         </div>
     </section>
 <?php endif; ?>
