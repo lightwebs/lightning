68b3212b188f5236f1be09b4743fdf149f60d528
Erik Näslund <contact@eriknaslund.com>
2023-01-03 11:30:15 +0100

    Disable full width for counter

--- inc/acf/lightning-builder/reg-components/text-media.php
+++ inc/acf/lightning-builder/reg-components/text-media.php
@@ -125,6 +125,15 @@ return [
             'wrapper' => [
                 'width' => '40',
             ],
+            'conditional_logic' => [
+                [
+                    [
+                        'field' => 'field_lb_text_media_type',
+                        'operator' => '!=',
+                        'value' => 'statistics',
+                    ],
+                ],
+            ],
         ],
         [
             'key' => 'field_lb_text_media_video_loop',
