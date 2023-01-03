ce4fd106de7b3d442a4234a04bf6540f0d481cc1
Alexander Tchernitchenko <alexander@lightweb.se>
2023-01-03 10:26:46 +0100

    Remove semi-colons

--- src/js/menu/hamburger.js
+++ src/js/menu/hamburger.js
@@ -2,26 +2,26 @@
 
 document.addEventListener('DOMContentLoaded', function() {
 
-    const hamburger = document.getElementById('main-menu-toggle');
-    const mobileMenu = document.getElementById('mobile-menu');
-    const burgerIcon = document.getElementById('burger-icon');
-    const burgerClose = document.getElementById('burger-close');
+    const hamburger = document.getElementById('main-menu-toggle')
+    const mobileMenu = document.getElementById('mobile-menu')
+    const burgerIcon = document.getElementById('burger-icon')
+    const burgerClose = document.getElementById('burger-close')
     
     function toggleMenu() {
-        mobileMenu.classList.toggle('!flex');
-        burgerIcon.classList.toggle('hidden');
-        burgerClose.classList.toggle('hidden');
-        document.body.classList.toggle('mobile-menu-open');
+        mobileMenu.classList.toggle('!flex')
+        burgerIcon.classList.toggle('hidden')
+        burgerClose.classList.toggle('hidden')
+        document.body.classList.toggle('mobile-menu-open')
     }
 
     hamburger.addEventListener('click', function() {
-        toggleMenu();
-    });
+        toggleMenu()
+    })
     
     // Close menu on resize to desktop (lg)
     window.addEventListener("resize", function() {
         if( document.body.classList.contains('mobile-menu-open') && window.innerWidth > 1024 ) {
-            toggleMenu();
+            toggleMenu()
         }
     })
 
