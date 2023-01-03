85622ffa3199bf39c5fda1dad611249633075187
Johan Carleborn <johancarleborn@gmail.com>
2023-01-03 16:18:00 +0100

    remove cursor pointer if whole text is showing

--- inc/cards/testimonial-card.php
+++ inc/cards/testimonial-card.php
@@ -14,12 +14,12 @@ if (!function_exists('testimonial_card')) {
         $content = get_the_content();
         $image_id = get_post_thumbnail_id($post_id);
 ?>
-        <div class="card group" data-post-id="<?php echo $post_id; ?>">
-            <div class="h-full border border-purple-100 p-4 md:p-6 flex flex-col gap-4 <?php echo $class; ?>">
+        <div class="card group testimonial" data-post-id="<?php echo $post_id; ?>">
+            <div class="relative h-full border border-purple-100 p-4 md:p-6 flex flex-col gap-4 <?php echo $class; ?>">
 
-                <div class="mb-2 cursor-pointer card-body line-clamp-5 xxl:line-clamp-4"><?php echo $content; ?></div>
+                <div class="mb-2 card-body line-clamp-5 xxl:line-clamp-4"><?php echo $content; ?></div>
 
-                <div class="flex items-center gap-4">
+                <div class="flex items-center gap-3 md:gap-4">
                     <?php echo image($image_id, 'thumbnail', 'object-cover aspect-square rounded-full border w-10 h-10') ?>
                     <div>
                         <p class="mb-0 text-sm font-semibold"><?php echo $title; ?></p>
