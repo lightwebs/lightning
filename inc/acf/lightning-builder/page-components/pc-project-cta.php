f724373f4ae4f707d605cfab153db4cfc0f8f216
Erik Näslund <contact@eriknaslund.com>
2023-01-04 11:38:15 +0100

    Remove depricated fields for custom_link

--- inc/acf/lightning-builder/page-components/pc-project-cta.php
+++ inc/acf/lightning-builder/page-components/pc-project-cta.php
@@ -6,7 +6,7 @@ if (get_row_layout() == 'project_cta' && !s(get_row_layout())['hide_component'])
     <section id="<?php echo s($prefix)['component_id']; ?>" class="pc-project_cta section  <?php echo section_spacing(); ?> <?php echo s($prefix)['bg_color']; ?>">
         <div class=" <?php echo s($prefix)['text_color']; ?> flex items-end w-full md:min-h-[360px]">
             <div class="container py-9 md:py-12 "><?php custom_link($link, '
-            text-4xl md:text-5xl m-0 uppercase md:w-1/2', true, 4); ?></div>
+            text-4xl md:text-5xl m-0 uppercase md:w-1/2'); ?></div>
         </div>
     </section>
 <?php endif; ?>
