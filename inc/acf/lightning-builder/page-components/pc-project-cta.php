0cf5eb994425b30e2cb8990e987e5d29ea8cffb0
Erik Näslund <contact@eriknaslund.com>
2023-01-04 20:23:33 +0100

    Hotfix missing md-statement

--- inc/acf/lightning-builder/page-components/pc-project-cta.php
+++ inc/acf/lightning-builder/page-components/pc-project-cta.php
@@ -7,7 +7,7 @@ if (get_row_layout() == 'project_cta' && !s(get_row_layout())['hide_component'])
 ?>
     <section id="<?php echo s($prefix)['component_id']; ?>" class="pc-project_cta section <?php echo s($prefix)['bg_color']; ?> ">
         <a href='<?php echo $link['url'] ?>' class='container block group <?php echo $is_small ? 'py-16' : 'pt-48 pb-12'; ?> h-full w-full <?php echo $link_color ?>' target='<?php echo $link['target'] ?>'>
-            <div class="max-w-[50%] h-full font-semibold text-4xl md:text-5xl <?php echo s($prefix)['text_color']; ?> ">
+            <div class="md:max-w-1/2 h-full font-semibold text-4xl md:text-5xl <?php echo s($prefix)['text_color']; ?> ">
                 <?php
                 echo $link['title']; ?>
 
