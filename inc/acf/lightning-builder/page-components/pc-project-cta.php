c207bed74fbb081f522b1ff0e5c584835b0964e8
Erik Näslund <contact@eriknaslund.com>
2023-01-04 15:45:06 +0100

    Add correct y-padding on larger screens

--- inc/acf/lightning-builder/page-components/pc-project-cta.php
+++ inc/acf/lightning-builder/page-components/pc-project-cta.php
@@ -6,7 +6,7 @@ if (get_row_layout() == 'project_cta' && !s(get_row_layout())['hide_component'])
 ?>
     <section id="<?php echo s($prefix)['component_id']; ?>" class="pc-project_cta section <?php echo s($prefix)['bg_color']; ?>">
         <div class=" <?php echo s($prefix)['text_color']; ?> flex items-end w-full <?php echo $is_small ? '' : 'md:min-h-[360px]'; ?>">
-            <div class="container py-9 md:py-12 "><?php custom_link($link, '
+            <div class="container py-9 md:py-[60px] "><?php custom_link($link, '
             text-4xl md:text-5xl m-0 uppercase md:w-1/2'); ?></div>
         </div>
     </section>
