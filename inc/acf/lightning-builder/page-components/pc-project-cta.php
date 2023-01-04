06dd5b3c09fd3aa6db5ecf47bc1254b6ead43906
Erik Näslund <contact@eriknaslund.com>
2023-01-04 12:03:20 +0100

    Add is_small acf

--- inc/acf/lightning-builder/page-components/pc-project-cta.php
+++ inc/acf/lightning-builder/page-components/pc-project-cta.php
@@ -2,6 +2,7 @@
 if (get_row_layout() == 'project_cta' && !s(get_row_layout())['hide_component']) :
     $prefix = get_row_layout();
     $link = get_sub_field('link');
+    $is_small = get_sub_field('cta_small');
 ?>
         <div class=" <?php echo s($prefix)['text_color']; ?> flex items-end w-full md:min-h-[360px]">
     <section id="<?php echo s($prefix)['component_id']; ?>" class="pc-project_cta section <?php echo s($prefix)['bg_color']; ?>">
