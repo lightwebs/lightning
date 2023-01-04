6309225be3850e8f86e2c4d960749a3630247a05
Erik Näslund <contact@eriknaslund.com>
2023-01-04 12:03:36 +0100

    Add min-height if small

--- inc/acf/lightning-builder/page-components/pc-project-cta.php
+++ inc/acf/lightning-builder/page-components/pc-project-cta.php
@@ -4,8 +4,8 @@ if (get_row_layout() == 'project_cta' && !s(get_row_layout())['hide_component'])
     $link = get_sub_field('link');
     $is_small = get_sub_field('cta_small');
 ?>
-        <div class=" <?php echo s($prefix)['text_color']; ?> flex items-end w-full md:min-h-[360px]">
     <section id="<?php echo s($prefix)['component_id']; ?>" class="pc-project_cta section <?php echo s($prefix)['bg_color']; ?>">
+        <div class=" <?php echo s($prefix)['text_color']; ?> flex items-end w-full <?php echo $is_small ? '' : 'md:min-h-[360px]'; ?>">
             <div class="container py-9 md:py-12 "><?php custom_link($link, '
             text-4xl md:text-5xl m-0 uppercase md:w-1/2'); ?></div>
         </div>
