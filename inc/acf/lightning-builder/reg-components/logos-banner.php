46c584a57a612e8aba6b5cbd93dacd118e7bf5d1
Erik Näslund <contact@eriknaslund.com>
2023-01-09 13:18:02 +0100

    Remove component head

--- inc/acf/lightning-builder/reg-components/logos-banner.php
+++ inc/acf/lightning-builder/reg-components/logos-banner.php
@@ -6,24 +6,6 @@ return [
     'display' => 'block',
     'sub_fields' => [
         [
-            'key' => 'field_lb_logos_banner_comp_head_tab',
-            'label' => 'Komponenthuvud',
-            'type' => 'tab',
-            'placement' => 'top',
-        ],
-        [
-            'key' => 'field_lb_logos_banner_header',
-            'label' => 'Header',
-            'name' => 'logos_banner',
-            'type' => 'clone',
-            'clone' => [
-                0 => 'group_clone_component_header',
-            ],
-            'display' => 'seamless',
-            'layout' => 'block',
-            'prefix_name' => 1,
-        ],
-        [
             'key' => 'field_lb_logos_banner_content_tab',
             'label' => 'Innehåll',
             'type' => 'tab',
