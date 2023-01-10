6c424b4058ff67de0351d4a76b4463a81502d5ed
Erik Näslund <contact@eriknaslund.com>
2023-01-10 16:06:32 +0100

    Update spacing on mobile

--- inc/acf/lightning-builder/page-components/pc-floating-cta.php
+++ inc/acf/lightning-builder/page-components/pc-floating-cta.php
@@ -3,7 +3,7 @@ $image = get_field('cta_image', 'option');
 $content = get_field('cta_content', 'option', false, false);
 $status = get_field('cta_status', 'option');
 ?>
-<div class="flex fixed bottom-10 items-center gap-4 right-10 cta-active z-[9999] hover:bg-white p-4 group">
+<div class="flex fixed bottom-2 right-2 md:bottom-10 items-center hover:gap-4 md:right-10 cta-active z-[9999] hover:bg-white p-4 group">
     <div class="relative shrink-0"><img src="<?php echo $image['url'] ?>" alt="<?php $image['alt'] ?>" class="h-20 w-20 object-cover rounded-full border-solid border-4 border-white">
         <span class="material-icons-round absolute -top-0 -right-2 bg-purple-500 p-1 rounded-full group-hover:hidden">
             waving_hand
