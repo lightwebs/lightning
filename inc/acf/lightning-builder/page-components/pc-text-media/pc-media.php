78885540248dbc5b46382ef31212b57356e6c671
Erik Näslund <contact@eriknaslund.com>
2023-01-18 12:54:03 +0100

    Prevent looping if set to false

--- inc/acf/lightning-builder/page-components/pc-text-media/pc-media.php
+++ inc/acf/lightning-builder/page-components/pc-text-media/pc-media.php
@@ -25,7 +25,7 @@ endif;
             $url = wp_get_attachment_url($video);
             $title = get_the_title($video);
         ?>
-            <video class="object-cover <?php echo $full_width ? 'h-full' : 'aspect-video'; ?>" <?php echo $video_controls ? ' controls' : ''; ?><?php echo $loop_video ? ' loop autoplay playsinline muted' : ''; ?>>
+            <video class="object-cover <?php echo $full_width ? 'h-full' : 'aspect-video'; ?>" <?php echo $video_controls ? ' controls' : ' autoplay muted playsinline'; ?><?php echo $loop_video ? ' loop autoplay playsinline muted' : ''; ?>>
                 <source src="<?php echo $url; ?>" type="video/mp4">
                 <source src="<?php echo $url; ?>" type="video/mp4">
             </video>
